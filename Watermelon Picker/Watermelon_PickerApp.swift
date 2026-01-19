d49df8e107d6f02951b7665875fb1763715b90a5
Hanqing Guo <18523978636@163.com>
2026-01-18 20:15:28 -1000

    Initial Commit

---
+++ Watermelon Picker/Watermelon_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watermelon_PickerApp.swift
+//  Watermelon Picker
+//
+//  Created by Hanqing Guo on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Watermelon_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
